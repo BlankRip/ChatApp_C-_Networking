62d820bd7ca55d9167f186c343cb704eb778d1a1
BlankRip <aniketh_g5@outlook.com>
2021-02-20 15:37:59 +0400

    Sever and cliend now unblocking sockets; can send message message and recievie one message per send

--- Chat App/Program.cs
+++ Chat App/Program.cs
@@ -11,37 +11,66 @@ namespace Chat_App
     class Program
     {
         static void Main(string[] args) {
-            if(args.Length > 0) {
+            List<Socket> clientSockets = new List<Socket>();
+            if (args.Length > 0) {
                 if(args[0] == "-server") {
                     Socket listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    listeningSocket.Blocking = false;
 
                     listeningSocket.Bind(new IPEndPoint(IPAddress.Any, 420));
                     Console.WriteLine("Waiting for connection...");
                     listeningSocket.Listen(10);
+                    while(true) {
+                        try {
+                            clientSockets.Add(listeningSocket.Accept());
+                        } catch (SocketException ex) {
+                            if(ex.SocketErrorCode != SocketError.WouldBlock)
+                                Console.WriteLine(ex);
+                        }
 
-                    Socket cliantSocket = listeningSocket.Accept();
-
-                    string strToSend = Console.ReadLine();
-                    cliantSocket.Send(ASCIIEncoding.ASCII.GetBytes("Hello Mustafa 1!\n" + strToSend));
-
-                    Byte[] receivedBuffer = new Byte[1024];
-                    cliantSocket.Receive(receivedBuffer);
-                    Console.WriteLine(ASCIIEncoding.ASCII.GetString(receivedBuffer));
-
+                        for (int i = 0; i < clientSockets.Count; i++)
+                        {
+                            try {
+                                Byte[] receivedBuffer = new Byte[1024];
+                                clientSockets[i].Receive(receivedBuffer);
+                                for (int j = 0; j < clientSockets.Count; j++) {
+                                    if (i != j)
+                                        clientSockets[j].Send(receivedBuffer);
+                                }
+                            } catch (SocketException ex) {
+                                if (ex.SocketErrorCode == SocketError.ConnectionAborted || ex.SocketErrorCode == SocketError.ConnectionReset) {
+                                    clientSockets[i].Close();
+                                    clientSockets.Remove(clientSockets[i]);
+                                }
+                                if (ex.SocketErrorCode != SocketError.WouldBlock && ex.SocketErrorCode != SocketError.ConnectionAborted
+                                    && ex.SocketErrorCode != SocketError.ConnectionReset)
+                                    Console.WriteLine(ex);
+                            }
+                        }
+                    }
                 } else if(args[0] == "-client") {
                     Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
                     Console.WriteLine("Connection...");
                     socket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 420));
                     Console.WriteLine("Connected to Server!!");
+                    socket.Blocking = false;
 
-                    Byte[] recievedBuffer = new Byte[1024];
-                    socket.Receive(recievedBuffer);
+                    while(true) {
+                        try {
+                            Console.Write("Type Now, Here: ");
+                            string strToSend = Console.ReadLine();
+                            socket.Send(ASCIIEncoding.ASCII.GetBytes(strToSend));
 
-                    Console.WriteLine(ASCIIEncoding.ASCII.GetString(recievedBuffer));
-
-                    string strToSend = Console.ReadLine();
-                    socket.Send(ASCIIEncoding.ASCII.GetBytes("Hello server 6969!\n" + strToSend));
+                            Byte[] recievedBuffer = new Byte[1024];
+                            socket.Receive(recievedBuffer);
+                            Console.WriteLine(ASCIIEncoding.ASCII.GetString(recievedBuffer));
+                        }
+                        catch (SocketException ex) {
+                            if (ex.SocketErrorCode != SocketError.WouldBlock)
+                                Console.WriteLine(ex);
+                        }
+                    }
                 }
             } else {
                 Console.WriteLine("Please provide arguments to this application either type -server or -client");
