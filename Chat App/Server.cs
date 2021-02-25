0800e1ed70cc4443d11eee58118da43d6d039d82
BlankRip <aniketh_g5@outlook.com>
2021-02-25 17:17:49 +0400

    Server Gives disconnected message && The App complete

--- Chat App/Server.cs
+++ Chat App/Server.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace Chat_App
 {
@@ -36,14 +37,32 @@ namespace Chat_App
                         Byte[] receivedBuffer = new Byte[1024];
                         int bytesRecieved = clientSockets[i].Receive(receivedBuffer);
 
+                        if(!dic.ContainsKey(clientSockets[i])) {
+                            Packet recieved = (Packet)Util.ByteArrayToObject(receivedBuffer);
+                            dic.Add(clientSockets[i], recieved.tag);
+                            Console.WriteLine($"{recieved.tag} just Connected");
+                        }
+
                         for (int j = 0; j < clientSockets.Count; j++) {
                             if (i != j)
                                 clientSockets[j].Send(receivedBuffer, bytesRecieved, SocketFlags.None);
                         }
                     } catch (SocketException ex) {
                         if (ex.SocketErrorCode == SocketError.ConnectionAborted || ex.SocketErrorCode == SocketError.ConnectionReset) {
+                            Socket disconnectedSocket = clientSockets[i];
+                            string tag = dic[disconnectedSocket];
+                            Console.WriteLine($"{tag} just Disconnected");
+
                             clientSockets[i].Close();
+                            dic.Remove(disconnectedSocket);
                             clientSockets.Remove(clientSockets[i]);
+
+                            Packet dcMessage = new Packet();
+                            dcMessage.message = $"Server: {tag} Disconnected.";
+                            dcMessage.txtColor = ConsoleColor.Red;
+
+                            for (int j = 0; j < clientSockets.Count; j++)
+                                clientSockets[j].Send(Util.ObjectToByteArray(dcMessage));
                         }
                         if (ex.SocketErrorCode != SocketError.WouldBlock && ex.SocketErrorCode != SocketError.ConnectionAborted
                             && ex.SocketErrorCode != SocketError.ConnectionReset)
